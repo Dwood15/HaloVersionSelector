896879f55050d8dc6b13130c4bc486f14fa30b63
Dason Woodhouse <dwood15@gmail.com>
2016-06-14 20:55:52 -0600

    First, Probably only commit
    I needed a better way to swap between Halo Custom Edition versions

---
+++ HaloVersionSelector/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace HaloVersionSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button108 = new System.Windows.Forms.Button();
+            this.button109 = new System.Windows.Forms.Button();
+            this.button110 = new System.Windows.Forms.Button();
+            this.sapien = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button108
+            // 
+            this.button108.Location = new System.Drawing.Point(12, 12);
+            this.button108.Name = "button108";
+            this.button108.Size = new System.Drawing.Size(101, 25);
+            this.button108.TabIndex = 0;
+            this.button108.Text = "Halo108";
+            this.button108.UseVisualStyleBackColor = true;
+            this.button108.Click += new System.EventHandler(this.button108_Click);
+            // 
+            // button109
+            // 
+            this.button109.Location = new System.Drawing.Point(119, 12);
+            this.button109.Name = "button109";
+            this.button109.Size = new System.Drawing.Size(101, 25);
+            this.button109.TabIndex = 1;
+            this.button109.Text = "Halo109";
+            this.button109.UseVisualStyleBackColor = true;
+            this.button109.Click += new System.EventHandler(this.button109_Click);
+            // 
+            // button110
+            // 
+            this.button110.Location = new System.Drawing.Point(226, 12);
+            this.button110.Name = "button110";
+            this.button110.Size = new System.Drawing.Size(101, 25);
+            this.button110.TabIndex = 2;
+            this.button110.Text = "Halo110";
+            this.button110.UseVisualStyleBackColor = true;
+            this.button110.Click += new System.EventHandler(this.button110_Click);
+            // 
+            // sapien
+            // 
+            this.sapien.Location = new System.Drawing.Point(119, 44);
+            this.sapien.Name = "sapien";
+            this.sapien.Size = new System.Drawing.Size(101, 25);
+            this.sapien.TabIndex = 3;
+            this.sapien.Text = "Sapien";
+            this.sapien.UseVisualStyleBackColor = true;
+            this.sapien.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 71);
+            this.Controls.Add(this.sapien);
+            this.Controls.Add(this.button110);
+            this.Controls.Add(this.button109);
+            this.Controls.Add(this.button108);
+            this.Name = "Form1";
+            this.Text = "Version Selector";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button108;
+        private System.Windows.Forms.Button button109;
+        private System.Windows.Forms.Button button110;
+        private System.Windows.Forms.Button sapien;
+
+    }
+}
+
---
+++ HaloVersionSelector/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+
+/*  Halo Version Selector
+ *      By Dwood
+ *  If you improve this, please share with me! I'd be very interested!
+ * */
+
+namespace HaloVersionSelector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string destination = "D:\\Program Files (x86)\\Microsoft Games\\Halo Custom Edition\\";
+        private string sourcefolder = "C:\\HaloVersions\\";
+        private string haloce = "haloce.exe";
+        private string strings = "strings.dll";
+        private string d3d9 = "d3d9.dll";
+        private string dinput = "dinput8.dll";
+
+        private void deleteDestinations()
+        {
+            File.Delete(destination + haloce);
+            File.Delete(destination + strings);
+            File.Delete(destination + d3d9);
+            File.Delete(destination + dinput);
+        }
+
+        private void button108_Click(object sender, EventArgs e)
+        {
+            deleteDestinations();
+            File.Copy(sourcefolder + "haloce108.exe", destination + haloce);
+            File.Copy(sourcefolder + "strings108.dll", destination + strings);
+            if(File.Exists(sourcefolder + "d3d9108.dll"))
+                File.Copy(sourcefolder + "d3d9108.dll", destination + d3d9);
+        }
+
+        private void button109_Click(object sender, EventArgs e)
+        {
+            deleteDestinations();
+            File.Copy(sourcefolder + "haloce109.exe", destination + haloce);
+            File.Copy(sourcefolder + "strings109.dll", destination + strings);
+            if(File.Exists(sourcefolder + "dinput8109.dll"))
+                File.Copy(sourcefolder + "dinput8109.dll", destination + dinput);
+        }
+
+        private void button110_Click(object sender, EventArgs e)
+        {
+            deleteDestinations();
+            File.Copy(sourcefolder + "haloce110.exe", destination + haloce);
+            File.Copy(sourcefolder + "strings110.dll", destination + strings);
+
+            if(File.Exists(sourcefolder + "dinput8110.dll"))
+                File.Copy(sourcefolder + "dinput8110.dll", destination + dinput);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.Delete(destination + d3d9);
+            File.Delete(destination + dinput);
+        }
+    }
+}
